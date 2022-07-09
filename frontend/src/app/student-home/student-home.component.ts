e2edaf994c8ae6512e3be3a72465d3f769e9cb3b
Ashraf-Sonde <ashrafsonde@gmail.com>
2022-07-09 12:18:17 +0530

    fixed the video link issue

--- frontend/src/app/student-home/student-home.component.ts
+++ frontend/src/app/student-home/student-home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/auth.service';
+import {WebRequestService} from '../shared/web-request.service';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+
 @Component({
   selector: 'app-student-home',
   templateUrl: './student-home.component.html',
@@ -8,17 +11,41 @@ import { AuthService } from '../shared/auth.service';
 })
 export class StudentHomeComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) {  }
+  videos: any = []
+  unsafeUrl: any
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private webRequestService: WebRequestService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.getVideos()
+  }
 
   ngOnInit(): void {
     var auth=null;
+    this.unsafeUrl = this.getSafeUrl('https://www.youtube.com/embed/tI_sdbnSw7c')
     auth=this.authService.getAccessToken();
     if (auth!==null) {
-      
+
     }
     else{
       this.router.navigate(['/login']);
     }
+  }
+
+  getSafeUrl(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  getVideos() {
+    return this.webRequestService.getVideos().subscribe((res: any) => {
+      this.videos = res;
+      for (let i=0; i<= res.length; i++) {
+        this.videos[i].videolink = this.getSafeUrl(this.videos[i].videolink)
+      }
+    });
   }
 
 }
